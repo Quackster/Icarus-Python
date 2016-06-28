af4873b6e47b355e25dd5803d074a4135604259f
Alex Miller <quacksterr.15@gmail.com>
2016-06-28 18:54:40 +1000

    Code comments

--- managers/clients/session.py
+++ managers/clients/session.py
@@ -18,6 +18,11 @@ class Session:
         self.details = Details()
 
     def send(self, data):
+        """
+        Send message to socket, this is passed through the message encoder class
+        :param data: object to send
+        :return:
+        """
         self.socket.send(message_encoder.encode(data))
 
     def close(self):
