b378f5d02c7527f8768eef1f373b0d9c17455c74
Alex Miller <quacksterr.15@gmail.com>
2016-07-01 01:41:29 +1000

    Source clean

--- managers/room/tasks/walking_task.py
+++ managers/room/tasks/walking_task.py
@@ -2,13 +2,22 @@
 Task to handle user walking
 author: Alex
 """
+
+
 class WalkingTask:
     def __init__(self, room):
         self.room = room
         self.delay = 0.5
 
     def run_task(self, coro=None):
+        """
+        Run walking task
+        :param coro: generator
+        :return:
+        """
 
+        # Only run when there's users in the room
         while len(self.room.get_players()) > 0:
-            print ("task run")
-            yield coro.sleep(0.5)
+
+            # Sleep task
+            yield coro.sleep(self.delay)
